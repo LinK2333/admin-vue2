49c3c7c507a706e8f9acfcd814002063562bd0a0
linkai <754706820@qq.com>
2021-10-22 16:31:54 +0800

    feat

--- src/mock/mockData.js
+++ src/mock/mockData.js
@@ -5,58 +5,57 @@
  * menus {菜单栏}
  */
 
-import Mock from 'mockjs'
-const userList = [
-  {
-    usersname: 'admin',
-    token: 'token1',
-    password: 123456,
-    introduction: '我是超级管理员',
-    role: ['admin'],
-    name: 'Super Admin',
-    uid: '001'
-  },
-  {
-    usersname: 'linkai',
-    token: 'token2',
-    password: 123456,
-    introduction: '我是林彦祖',
-    role: ['common'],
-    name: 'User',
-    uid: '002'
-  }
-    ]
+ import Mock from 'mockjs'
+ const userList = [
+   {
+     usersname: 'admin',
+     token: 'token1',
+     password: 123456,
+     introduction: '我是超级管理员',
+     role: ['admin'],
+     name: 'Super Admin',
+     uid: '001'
+   },
+   {
+     usersname: 'linkai',
+     token: 'token2',
+     password: 123456,
+     introduction: '我是林彦祖',
+     role: ['common'],
+     name: 'User',
+     uid: '002'
+   }
+     ]
 
-export default {
-    /**
-     * 验证登录
-     * 返回token值
-     */
-    login: config => {
-      const data = JSON.parse(config.body);
-      const { username, password } = data
-      const isUser = userList.some(item => item.usersname == username)
-      const result = userList.find(item => { return item.usersname == username && item.password == password })
-        if (!isUser) {
-            return { status: 1000, msg: '用户不存在', result: '' }
-        } else if (userList.some(item => { return item.usersname == username && item.password == password })) {
-            return { status: 200, msg: '登录成功', result: result.token }
-        } else {
-            return { status: 1001, msg: '密码错误', result: '' }
-        }
-  },
-    /**
-     * 拉取用户信息
-     * 返回用户信息，角色
-     */
-    getInfo: config => {
-      const data = config.body
-      const result = userList.find(item => item.token == data)
-      if (userList.some(item => item.token == data)) {
-        return { status : 200, text: '拉取用户信息成功' , result: {userList: result , role: result.role} }
-      }
-    },
-    // 退出登录
-    loginOut: config => { return { status: 200, text: '退出登录成功', result: '' } },
-
-}
+ export default {
+     /**
+      * 验证登录
+      * 返回token值
+      */
+     login: config => {
+       const data = JSON.parse(config.body);
+       const { username, password } = data
+       const isUser = userList.some(item => item.usersname == username)
+       const result = userList.find(item => { return item.usersname == username && item.password == password })
+         if (!isUser) {
+             return { status: 1000, msg: '用户不存在', result: '' }
+         } else if (userList.some(item => { return item.usersname == username && item.password == password })) {
+             return { status: 200, msg: '登录成功', result: result.token }
+         } else {
+             return { status: 1001, msg: '密码错误', result: '' }
+         }
+   },
+     /**
+      * 拉取用户信息
+      * 返回用户信息，角色
+      */
+     getInfo: config => {
+       const data = config.body
+       const result = userList.find(item => item.token == data)
+       if (userList.some(item => item.token == data)) {
+         return { status : 200, text: '拉取用户信息成功' , result: {userList: result , role: result.role} }
+       }
+     },
+     // 退出登录
+     loginOut: config => { return { status: 200, text: '退出登录成功', result: '' } },
+ }
--- src/mock/test.js
+++ src/mock/test.js
@@ -3,12 +3,12 @@
  * 配置模拟接口
  * @module mock
  */
-import Mock from 'mockjs'
-import mockData from './mockData'
-// 开发环境
-Mock.mock('http://localhost:8080/loginManage', 'post', mockData.login) // 登录验证返回
-Mock.mock('http://localhost:8080/getInfo', 'post', mockData.getInfo) // 拉取用户信息
-Mock.mock('http://localhost:8080/loginout', 'get', mockData.loginOut) // 退出登录
-Mock.mock('http://localhost:8080//menus', 'post', mockData.menus) // 菜单栏
+ import Mock from 'mockjs'
+ import mockData from './mockData'
+ // 开发环境
+ Mock.mock('http://localhost:8080/loginManage', 'post', mockData.login) // 登录验证返回
+ Mock.mock('http://localhost:8080/getInfo', 'post', mockData.getInfo) // 拉取用户信息
+ Mock.mock('http://localhost:8080/loginout', 'get', mockData.loginOut) // 退出登录
+ Mock.mock('http://localhost:8080//menus', 'post', mockData.menus) // 菜单栏
 
-export default Mock
+ export default Mock
